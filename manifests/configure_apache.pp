a0acf83584e72fd2246cef21aa69c6067d4f0500
root <root@epbyminw3088puppet.(none)>
2014-09-23 07:41:44 +0000

    apache restart added

--- manifests/configure_apache.pp
+++ manifests/configure_apache.pp
@@ -3,7 +3,11 @@ class configure_apache {
 file {'/etc/httpd/conf.d/tomcat-vhost.conf':
    ensure  => file,
    content => template('exittask/tomcat-vhost.erb'),
-   }
+   notify => exec['apache_reload'], 
+  }
 
+exec {'apache_reload':
+command => '/etc/init.d/httpd graceful'
+}
 
 }
